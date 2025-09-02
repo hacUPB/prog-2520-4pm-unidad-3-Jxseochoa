88946c1a99d36d30efc0cc447b0b6050cfef9f3b
Jose Ochoa <jose.ochoac@upb.edu.co>
2025-09-02 17:22:02 -0500

    Ejercicio for.

--- bucles/buclefor.py
+++ bucles/buclefor.py
@@ -1,7 +1,7 @@
 mensaje = "Programación con Phyton"
 numero = int(input("Ingrese  el número: "))
 
-contador = int(input("Ingrese un número:")
+contador = int(input("Ingrese un número:"))
 suma = 0
 for contador in range(1, contador + 1):
     if contador % 2 == 0:
